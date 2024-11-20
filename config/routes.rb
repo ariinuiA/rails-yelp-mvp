9687ac72ee4c063e7bf30ec12fb9ceea0b95537e
Gabriel A <ariinuitahiti@gmail.com>
2024-11-20 15:54:07 -0500

    Can leave a review to a restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :restaurants, except: %i[destroy edit update]
+  resources :restaurants, except: %i[destroy edit update] do
+    resources :reviews, only: %i[new create]
+  end
 end
