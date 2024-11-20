33733808f3249dcea3ad838c3e0c0c740aa64424
Gabriel A <ariinuitahiti@gmail.com>
2024-11-20 12:54:45 -0500

    added 5 random restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+restaurants = ["Mcdonald", "Starbucks", "Subway", "Burger King", "KFC"]
+addresses = ["111 Villeneuve", "222 Laurier", "333 Joliette", "444 Frontenac", "555 Place Des Arts"]
+phone_numbers = ["+33 14521452", "+789 456 123", "+44 41741741", "+1 198745622", "+689 1478963"]
+categories = %w[chinese italian japanese french belgian]
+
+restaurants.each_with_index do |restaurant, index|
+  r = Restaurant.new(
+    name: restaurant,
+    address: addresses[index],
+    phone_number: phone_numbers[index],
+    category: categories[index]
+  )
+  r.save
+end
